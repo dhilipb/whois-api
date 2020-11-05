37b1027eca0af1084189e97984533c9d257ffd0d
Dhilip Buddha <dhilip.buddha@blackrock.com>
2020-11-05 23:29:19 +0000

    Add path

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const app = express()
+const _ = require("lodash");
+const express = require("express");
+const app = express();
 
+var whois = require("./whois");
 
-var whois = require('./whois');
+app.get("/api/", (req, res) => {
+    const domain = req.query.domain;
+    const path = req.query.path;
 
+    whois.whois(domain, function (err, data) {
+        if (err) {
+            res.json(err);
+            return;
+        }
 
-app.get('/api/', (req, res) => {
+        if (path) {
+          res.json(_.get(data, path));
+        } else {
+          res.json(data);
+        }
+    });
+});
 
-  const domain = req.query.domain
-
-  whois.whois(domain, function (err, data) {
-    if(err) {
-      res.json(err);
-      return;
-    }
-    res.json(data);
-  });
-})
-
-
-
-app.listen(3000, () => console.log('App listening on port http://localhost:3000!'));
+app.listen(3000, () => console.log("App listening on port http://localhost:3000! Api at /api/"));
